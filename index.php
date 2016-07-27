fc1a75aacabd9ed3274e43ca6d0b861cd7c72493
Jabrayil <cabi10@mail.ru>
2016-07-27 16:18:31 +0400

    Reg and Index
    Added the ability to upload images. Began work on index page

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
-﻿<form method="POST" action="check.php">
-	<span>Имя</span>
-	<input type="text" name="name">
-	<button>Войти</button>
-	<a href="#">Забыли пароль?</a>
-</form>
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title>Tvoyazona.ru - Добро пожаловать на наш развлекательный портал!</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link href='https://fonts.googleapis.com/css?family=PT+Sans&subset=cyrillic,latin' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="main">
+	<header>
+		<ul>
+			<li><a href="#">ЛУЧШЕЕ</a></li>
+			<li><a href="#">ГОРЯЧЕЕ</a></li>
+		</ul>
+
+		<a href="#" class="auth">Регистрация</a>
+		<a href="#" class="auth">Авторизация</a>
+	</header>
+</div>
+</body>
+</html>
---
+++ user.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+require_once('connect.php');
+
+
+$sql = "SELECT * FROM `users`";	
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo "<table border=1><tr><th>User ID</th>	<th>Name</th>	<th>Pass</th>	<th>Email</th>	<th>Avatar</th>	<th>Block</th>	<th>Group_id</th></tr>	<tr><td>";
+		echo $row['user_id']."</td><td>";
+		echo $row['name']."</td><td>";
+		echo $row['pass']."</td><td>";
+		echo $row['email']."</td><td>";
+		echo $row['avatar']."</td><td>";
+		echo $row['block']."</td><td>";
+		echo $row['group_id']."</td></tr>";
+	}
+} else {
+	echo "0 results";
+}
+
+?>
